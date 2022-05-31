24b03bbc8313bb9b84ebff33463ceb088789b732
mohammad-nassar10 <mohammad.nassar@ibm.com>
2022-05-31 17:36:00 +0300

    Add python code to drop table.
    Signed-off-by: mohammad-nassar10 mohammad.nassar@ibm.com

---
+++ clean-table.py
@@ -0,0 +1,22 @@
+import trino
+
+def execute_query(cur, query):
+    cur.execute(query)
+    # res = None
+    try:
+        res = cur.fetchall()
+        return res
+    except Exception as e:
+        print(e)
+    # return res
+
+def connect_user(username, catalog):
+    conn = trino.dbapi.connect(host='host.docker.internal', port=8080, user=username, catalog=catalog)
+    cur = conn.cursor()
+    return cur
+
+if __name__ == "__main__":
+    print("show catalogs")
+    cur = connect_user("admin", "iceberg")
+    res = execute_query(cur, "SHOW CATALOGS")
+    print(res)
